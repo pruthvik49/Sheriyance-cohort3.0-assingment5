e0b84a470dde2bed7aad894d6539cc469f6b1cb8
pruthvik49 <pruthvirajbhoir070@gmail.com>
2026-06-21 09:24:24 +0530

    adding more fetures

--- mini project to do list/index.js
+++ mini project to do list/index.js
@@ -1,68 +1,65 @@
  const btn = document.querySelector('#enter')
  const taskN = document.querySelector(".taskname")
- const content= document.querySelector(".input")
+ const content = document.querySelector(".input")
  const renderDiv = document.querySelector(".Text_area")
 
-const addAllTaksn =  [];
+const addAllTaksn =  JSON.parse(localStorage.getItem("alltask")) || [];
 
-// to render
-function taskbulets (){
-    renderDiv.innerHTML = ''
-    let title = taskN.value.trim();
-    let con = content.value.trim();
-    if (taskN.value === "") return;
-    addAllTaksn.push({title , con})
-    JSON.parse(localStorage.getItem("alltask"))
-    // localStorage.clear()
-let taskkeys = {
-    t : taskN.value,
-    para:content.value
-    
-   }
-    addAllTaksn.forEach((e,id)=>{
-  const taskdiv = document.createElement('div')
-   taskdiv.innerHTML += ` <div id=${id} class="taks">
-        <div class="tasktop">
-            <h4>${taskkeys.t}</h4>
-            <div>
-                <img class="delet" src="./imgs/delet.svg" alt="">
-                <img class="edite" src="./imgs/edit-circle-fill.svg" alt="">
-            </div>
-        </div>
-        <div class="taskbottom">
-            ${taskkeys.para}
-        </div>
+// // to render
+// function taskbulets (){
+//     renderDiv.innerHTML = ""
 
-    </div>`
-    renderDiv.appendChild(taskdiv)
-    localStorage.setItem("alltask" ,JSON.stringify(addAllTaksn));
-      
-})
-   taskN.value = ""
-   content.value = ""
-} 
+//     addAllTaksn.forEach((e,id)=>{
+//         const taskdiv = document.createElement('div')
+//         taskdiv.innerHTML = ` <div id=${id} class="taks">
+//             <div class="tasktop">
+//                 <h4>${e.title}</h4>
+//                 <div>
+//                     <img class="delet" src="./imgs/delet.svg" alt="">
+//                     <img class="edite" src="./imgs/edit-circle-fill.svg" alt="">
+//                 </div>
+//             </div>
+//             <div class="taskbottom">
+//                 ${e.para}
+//             </div>
+//         </div>`
+//         renderDiv.appendChild(taskdiv)
+//     })
+//     localStorage.setItem("alltask" ,JSON.stringify(addAllTaksn));
+// }
+
+taskbulets();
+
 btn.addEventListener("click", function(e) {
-    e.preventDefault();
-    
-   taskbulets()
-   
+   let title = taskN.value.trim();
+let para = content.value.trim();
+
+if (!title) return;
+if (!para) return;
+addAllTaksn.push({title , para})
+
+taskbulets();
+
+taskN.value = "";
+content.value = "";
 })
-
-
 
 // for enter input 
-taskN.addEventListener("keydown",(e)=>{
-    if(e.key === 'Enter'){
+taskN.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
         e.preventDefault();
-   taskbulets()
+        btn.click();
     }
-})
-content.addEventListener("keydown",(e)=>{
-    if(e.key === 'Enter'){
+});
+
+content.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
         e.preventDefault();
-   taskbulets()
+        btn.click();
     }
-})
+});
+
+// render saved tasks when page loads
 
 
 
