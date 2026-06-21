093ae67dfdaea144b8b5045b552ed73e0c7b4716
pruthvik49 <pruthvirajbhoir070@gmail.com>
2026-06-21 10:57:32 +0530

    adding more fetures

--- mini project to do list/index.js
+++ mini project to do list/index.js
@@ -1,34 +1,43 @@
  const btn = document.querySelector('#enter')
  const taskN = document.querySelector(".taskname")
+ const editbtn = document.querySelector('#greebtn')
  const content = document.querySelector(".input")
+ const form = document.querySelector(".editeform")
  const renderDiv = document.querySelector(".Text_area")
 
 const addAllTaksn =  JSON.parse(localStorage.getItem("alltask")) || [];
 
-// // to render
-// function taskbulets (){
-//     renderDiv.innerHTML = ""
+// to render
+function taskbulets (){
+    renderDiv.innerHTML = ""
 
-//     addAllTaksn.forEach((e,id)=>{
-//         const taskdiv = document.createElement('div')
-//         taskdiv.innerHTML = ` <div id=${id} class="taks">
-//             <div class="tasktop">
-//                 <h4>${e.title}</h4>
-//                 <div>
-//                     <img class="delet" src="./imgs/delet.svg" alt="">
-//                     <img class="edite" src="./imgs/edit-circle-fill.svg" alt="">
-//                 </div>
-//             </div>
-//             <div class="taskbottom">
-//                 ${e.para}
-//             </div>
-//         </div>`
-//         renderDiv.appendChild(taskdiv)
-//     })
-//     localStorage.setItem("alltask" ,JSON.stringify(addAllTaksn));
-// }
+    addAllTaksn.forEach((e,id)=>{
+        const taskdiv = document.createElement('div')
+        taskdiv.innerHTML = ` <div id=${id} class="taks">
+            <div class="tasktop">
+                <h4>${e.title}</h4>
+                <div>
+                    <img class="delet" src="./imgs/delet.svg" alt="">
+                    <img id = "greebtn" class="edite" src="./imgs/edit-circle-fill.svg" alt="">
+                </div>
+            </div>
+            <div class="taskbottom">
+                ${e.para}
+            </div>
+        </div>`
+        renderDiv.appendChild(taskdiv)
+    })
+    
+    
+    editbtn.addEventListener("click",()=>{
+
+            form.style.display = 'flex'
+        })
+    localStorage.setItem("alltask" ,JSON.stringify(addAllTaksn));
+}
 
 taskbulets();
+
 
 btn.addEventListener("click", function(e) {
    let title = taskN.value.trim();
